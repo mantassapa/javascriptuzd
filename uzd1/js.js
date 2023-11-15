3d30dcf55ae1437a3de6c806a2c580710cb2875b
Moksleivis <Moksleivis@kitm.local>
2023-11-15 13:59:49 +0200

    half

---
+++ uzd1/js.js
@@ -0,0 +1,41 @@
+//pirma uzduotis
+let p =10
+
+if (3>=p && p>=1){console.log('neigiamas pazimys');}
+else if (5>=p&&p>=4)console.log('teigiams bet reikia pasistenkti')
+else if (7>=p&&p>=6)console.log('vidutiniskai, ar tau to uztenka?')
+else if (9>=p&&p>=8)console.log('pagirk save')
+else if (p==10)console.log('Super!')
+else{console.log('pateikta netinkama reiksme')}
+
+//antra uzduotis
+men = 'kovas'
+switch(men.toLowerCase()){
+    case 'gruodis':
+    case 'sausis':
+    case 'vasaris':
+        console.log('ziema');break;
+    case 'kovas':
+    case 'balandis':
+    case 'geguze':
+        console.log('pavasaris');break;
+    case 'birzelis':
+    case 'liepa':
+    case 'rugpjutis':
+        console.log('vasara');break;
+    case 'rugsejis':
+    case 'spalis':
+    case 'lapkritis':
+        console.log('ruduo');break;
+}
+//trecia uzduotis
+
+let h =6
+
+if (11>=h && h>=6){console.log('Labas rytas');}
+else if (18>=h&&h>=12)console.log('laba diena')
+else if (21>=h&&h>=19)console.log('Labas vakaras')
+else if (5>=h||h>=22)console.log('Labanakt')
+else{console.log('pateikta netinkama reiksme')}
+
+//ketvirta uzduotis
